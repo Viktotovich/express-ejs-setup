b486b2cda6f312583b4d4bf2db12f810226443fb
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-01-30 22:37:06 +0400

    Set up a sample view, still reading and understanding the docs res.locals and app.locals

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("node:path");
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { message: "Ejs is awesome!" });
+});
+
+app.get("/throw", (req, res) => {
+  throw new Error("Intentional Failure");
+});
+
+app.listen(PORT, () => {
+  console.dir(`We are now live on PORT: ${PORT}`);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(400).send(`<h2>Unexpected Error: ${err.message}</h2>`);
+});
